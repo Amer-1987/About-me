9136f678a1cacd7e5d8e34806055e32c1291e84f
Amer-1987 <amer_1987_99@yahoo.com>
2021-07-07 23:58:05 +0300

    lab04

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let score=0;
 let userName = prompt("what is your name?");
 console.log("userName");
 alert("welcome " + userName + " in our project");
@@ -10,6 +10,7 @@ favourite = favourite.toLowerCase();
 if (favourite === "yes" || favourite === "y") {
     console.log(favourite + " I like sport because it is very useful and meaningful");
     alert("that is true, I like it");
+    score++;
 }
 else if (favourite === "no" || favourite === "n") {
     console.log("I like sport because it is very useful and meaningful");
@@ -22,6 +23,8 @@ typeOfSport = typeOfSport.toLowerCase();
 if (typeOfSport === "yes" || typeOfSport === "y") {
     console.log(typeOfSport + ", I like football, It is the greatest game in the world, keep in mind that The world\'s richest people are soccer players");
     alert("that is true, I like football, It is the greatest game in the world");
+    score++;
+
 }
 else if (typeOfSport === "no" || typeOfSport === "n") {
     console.log(", keep in mind that The world\'s richest people are soccer players");
@@ -35,6 +38,8 @@ club = club.toLowerCase();
 if (club === "yes" || club === "y") {
     console.log(club + ", Real Madrid is the most popular club in the world with 100 million Instagram followers");
     alert("Yes Real Madrid is the most popular club in the world with 100 million Instagram followers");
+    score++;
+
 } else if (club === "no" || club === "n") {
     console.log("actually, Real Madrid is my favourite club");
     alert("actually, Real Madrid is my favourite club");
@@ -46,6 +51,8 @@ myCharecture = myCharecture.toLowerCase();
 if (myCharecture === "yes" || myCharecture === "y") {
     console.log(myCharecture + ", that explain why I am in this course at this stage of my life");
     alert('That explain why I am in this course at this stage of my life, and I wish success and develop myself more and more');
+    score++;
+
 }
 else if (myCharecture === "no" || myCharecture === "n") {
     console.log("This contradicts with my being here")
@@ -59,6 +66,8 @@ mark = mark.toLowerCase();
 if (mark === "yes" || mark === "y") {
     console.log(mark + ", I agree with you and I deserve that");
     alert('I agree with you, I deserve that');
+    score++;
+
 }
 else if (mark === "no" || mark === "n") {
     console.log("could you reconsidered again,please?");
@@ -88,6 +97,7 @@ for (i = 0; i < 4; i++) {
     else {
         alert('correct guess, that is great');
         console.log("Your guess is " + Guess + ", correct, that is great");
+        score++;
 
         break;
     }
@@ -104,22 +114,34 @@ let Movies = ['vikings', 'blacklist', 'spartacus', 'mentalist', 'prison break', 
 console.log(Movies.length);
 
 
-let favMovies = prompt('');
+let favMovies;
 
 for (let i = 0; i < 6; i++) {
-
     favMovies = prompt('what are my favourite movies?');
+
     for (let j = 0; j < 6; j++) {
         console.log('second loop', j);
 
         if (favMovies === Movies[j]) {
             console.log(Movies[j]);
             alert("correct");
-            break;
+            score++;
+
+            //break;
+            i = 10;
         }
-        else {
-             alert("wrong");
-             favMovies = prompt('what are my favourite movies?');
-        }
+        // else {
+        //     alert("wrong");
+        //     favMovies = prompt('what are my favourite movies?');
+        // }
     }
+
+     if (i===5){
+         alert("sorry no more tries")
+     }  else if (i<6){
+         alert("wrong answer")
+     }
+
 }
+alert("My favourite Movies are vikings , blacklist, spartacus, mentalist, prison break and breaking bad");
+alert(`thank you ${userName},your score is ${score}/7`);
